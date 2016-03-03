f21dc62a92642d47875925614560309670c06192
Oleksandr Zinenko <zinenko@lri.fr>
2016-03-03 13:14:37 +0100

    Add forgotten files

---
+++ beta.cc
@@ -0,0 +1,23 @@
+#include <clay/array.h>
+
+#include <algorithm>
+#include <cstdlib>
+
+clay_array_p chlore_beta_prefix(clay_array_p a1, clay_array_p a2) {
+  clay_array_p prefix;
+  int i, e;
+
+  if (!a1 || !a2)
+    return NULL;
+
+  prefix = clay_array_malloc();
+  for (i = 0, e = std::min(a1->size, a2->size); i < e && a1->data[i] == a2->data[i]; i++)
+    ;
+  if (prefix->size < i) {
+    prefix->size = i;
+    prefix->data = (int *) realloc(prefix->data, i * sizeof(int));
+  }
+  memcpy(prefix->data, a1->data, i * sizeof(int));
+  return prefix;
+}
+
---
+++ beta.h
@@ -0,0 +1,7 @@
+#ifndef BETA_H
+#define BETA_H
+
+clay_array_p chlore_beta_prefix(clay_array_p a1, clay_array_p a2);
+
+#endif // BETA_H
+
